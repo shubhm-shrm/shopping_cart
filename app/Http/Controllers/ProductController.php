4a64e52d29b790a1c28957409e496519b7e6aaef
Shubham Sharma <shubh.shrm95@gmail.com>
2017-06-12 00:57:22 +0530

    placing Order and Cart functionality implemented

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Product;
+use App\Cart;
+use App\Order;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+use Stripe\Stripe;
+use Stripe\Charge;
+class ProductController extends Controller
+{
+	public function getIndex()
+	{
+		$products =  Product::all();
+		return view('shop.index',['products'=>$products]);
+	}
+
+	public function getAddToCart(Request $request,$id)
+	{
+		$product = Product::find($id);
+
+		$oldCart = Session::has('cart') ? Session::get('cart') : null;
+		$cart = new Cart($oldCart);
+		$cart->add($product,$product->id);
+
+		Session::put('cart',$cart);
+	//	$request->session()->put('cart')
+	//	dd($request->session()->get('cart'));
+		return redirect()->route('product.index');
+	}
+
+	public function getReduceByOne($id)
+	{
+		$oldCart = Session::has('cart') ? Session::get('cart') : null;
+		$cart = new Cart($oldCart);
+		$cart->reduceByOne($id);
+		if(count($cart->items) >0){
+			Session::put('cart',$cart);
+		}
+		else{
+			Session::forget('cart');
+		}
+		return redirect()->route('product.shoppingCart');
+	}
+
+	public function getReduceItem($id)
+	{
+		$oldCart = Session::has('cart') ? Session::get('cart') : null;
+		$cart = new Cart($oldCart);
+		$cart->removeItem($id);
+
+		if(count($cart->items) >0){
+			Session::put('cart',$cart);
+		}
+		else{
+			Session::forget('cart');
+		}
+		return redirect()->route('product.shoppingCart');
+	}
+
+	public function getCart()
+	{
+		if(!Session::has('cart'))
+		{
+			return view('shop.shopping-cart');
+		}
+		else
+		{
+			$oldCart = Session::get('cart');
+			$cart = new Cart($oldCart);
+			return view('shop.shopping-cart',['products'=> $cart->items,'totalPrice' => $cart->totalPrice ]);			
+		}
+	}
+
+	public function getCheckout()
+	{
+		if(!Session::has('cart'))
+		{
+			return view('shop.shopping-cart');
+		}
+		else
+		{
+			$oldCart = Session::get('cart');
+			$cart = new Cart($oldCart);
+			$total = $cart->totalPrice;
+			return view('shop.checkout',['total'=> $total]);			
+		} 
+	}
+
+	public function postCheckout(Request $request)
+	{
+		if(!Session::has('cart'))
+		{
+			return redirect()->route('product.shopping-cart');
+		}
+		else
+		{
+			$oldCart = Session::get('cart');
+			$cart = new Cart($oldCart);
+			Stripe::setApiKey('sk_test_bM8IfpgVbZmzojyx03WrdRIa');
+			try{
+				$charge = Charge::create(array(
+					"amount" => $cart->totalPrice * 100,
+					"currency" => "usd",
+  					"source" => $request->input('stripeToken'), // obtained with Stripe.js
+  					"description" => "Test Charge"
+  					));
+				$order = new Order();
+				$order->cart = serialize($cart);
+				$order->address = $request->input('address');
+				$order->name = $request->input('name');
+				$order->payment_id = $charge->id;	
+				
+				Auth::user()->orders()->save($order);							
+			}
+			catch(\Exception$e){
+				return redirect()-> route('checkout')->with('error',$e-getMessage());
+			}
+			Session::forget('cart');
+			return redirect()->route('product.index')->with('success','Successfully Purchased Products.!');
+		}
+	}
+}
